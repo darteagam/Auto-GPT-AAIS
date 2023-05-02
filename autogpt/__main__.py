540697732151ac2bc05d3742bfdc70b99484d3ff
Daniel Arteaga <darteagam15@gmail.com>
2023-05-01 23:23:57 -0500

    prompt modification

--- autogpt/__main__.py
+++ autogpt/__main__.py
@@ -1,5 +1,11 @@
-"""Auto-GPT: A GPT powered AI Assistant"""
-import autogpt.cli
+# """Auto-GPT: A GPT powered AI Assistant"""
+# import autogpt.cli
+
+# if __name__ == "__main__":
+#     autogpt.cli.main()
+
+"""AAIA: Advanced AI Assistant"""
+import autogpt.ncli
 
 if __name__ == "__main__":
-    autogpt.cli.main()
+    autogpt.ncli.main()
